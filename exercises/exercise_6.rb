133410c467a95bb40b72e51ccb7528fe6ebd9292
Ziyu Huang <aprilhuang825@gmail.com>
2022-07-04 19:12:49 +0000

    add files

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Mujtaba", last_name: "Bauer", hourly_rate: 50)
+@store1.employees.create(first_name: "Faith", last_name: "Solomon", hourly_rate: 65)
+@store2.employees.create(first_name: "Tayyibah", last_name: "Colley", hourly_rate: 60)
+@store2.employees.create(first_name: "Myron", last_name: "Legge", hourly_rate: 60)
+@store2.employees.create(first_name: "Alaina", last_name: "Arellano", hourly_rate: 70)
